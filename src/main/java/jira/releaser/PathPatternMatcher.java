fe0dacb4fe250e91f2c4efcd48d325846a9936ed
David Pärsson <david.parsson@gmail.com>
2012-07-12 09:16:15 +0200

    Renamed constant

--- src/main/java/jira/releaser/PathPatternMatcher.java
+++ src/main/java/jira/releaser/PathPatternMatcher.java
@@ -4,10 +4,10 @@ import java.util.regex.Pattern;
 
 public class PathPatternMatcher {
 
-    private static final String RELEASE_PATTERN = "([^/]+/)?trunk/.*";
+    private static final String TRUNK_PATTERN = "([^/]+/)?trunk/.*";
 
     public static boolean isInTrunk(final String path) {
-        return Pattern.matches(RELEASE_PATTERN, path);
+        return Pattern.matches(TRUNK_PATTERN, path);
     }
 
 }
